1e8da63faaf462d04c18f31e1677232481ded602
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-22 20:44:13 +0530

    added the compoenents

--- src/Frontend/components/navigationBar.js
+++ src/Frontend/components/navigationBar.js
@@ -1,12 +1,18 @@
 import "../style/navigationBar.css"
+import title from "../assets/navbarimage.png"
 const NavigationBar = () => {
     return ( 
         <div className="navigationBar-container">
             <div className="navigation-title">
-
+                <img src={title} alt="" />
+                Case Cracker
             </div>
             <div className="navigation-components">
-
+                <span className="nav">Home</span>
+                <span className="nav">pages</span>
+                <span className="nav">Case Studies</span>
+                <span className="nav">Blogs</span>
+                <span className="nav">Contact</span>
             </div>
             <div className="about-buttons">
 
