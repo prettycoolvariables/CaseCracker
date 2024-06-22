31047a60b3d93dff2e12d65a4f30292d891d78b3
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-06-22 20:15:47 +0530

    about to make the navigaiton bar

---
+++ src/Frontend/components/navigationBar.js
@@ -0,0 +1,10 @@
+import "../style/navigationBar.css"
+const NavigationBar = () => {
+    return ( 
+        <div className="navigationBar-container">
+            helo
+        </div>
+     );
+}
+ 
+export default NavigationBar;
